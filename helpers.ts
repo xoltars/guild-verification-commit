002e5a783688ed497c999224b569b4c1512f1ee0
xoltars <116966504+xoltars@users.noreply.github.com>
2026-03-06 01:03:52 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:03:50.851Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T22:03:53.071Z
+Commit: Fix memory leak in event handlers
