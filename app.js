b79929d5892fd1481c55a6c4dd1d8af75a719fc3
xoltars <116966504+xoltars@users.noreply.github.com>
2026-03-06 01:02:58 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:02:55.292Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T22:02:58.565Z
+Commit: Fix broken links in documentation
