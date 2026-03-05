069dee2c8b35657a7ba92343244d44fe5a78f736
xoltars <116966504+xoltars@users.noreply.github.com>
2026-03-06 01:03:13 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:03:06.087Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T22:03:14.007Z
+Commit: Correct typo in configuration file
