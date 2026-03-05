d0dabe399a4f2545ad7c7a996835a45f7daec91f
xoltars <116966504+xoltars@users.noreply.github.com>
2026-03-06 01:04:03 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:03:57.428Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T22:04:03.549Z
+Commit: Fix CSS styling bug on mobile devices
