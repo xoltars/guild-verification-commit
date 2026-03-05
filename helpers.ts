74464bb254abc8559f06d4cc3affc635d6bf8a82
xoltars <116966504+xoltars@users.noreply.github.com>
2026-03-06 01:03:30 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:03:14.007Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T22:03:30.652Z
+Commit: Fix broken links in documentation
