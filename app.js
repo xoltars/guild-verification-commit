5067a2a0da52a2ecdc813b88e8e3dab8297b6d62
xoltars <116966504+xoltars@users.noreply.github.com>
2026-03-06 01:03:24 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:03:09.387Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T22:03:25.093Z
+Commit: Fix memory leak in event handlers
